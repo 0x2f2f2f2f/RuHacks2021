7de5bac55ca2cbd846e754052db3586dbef7a0be
Kai Xu <kaichengxu12@hotmail.com>
2021-05-01 14:19:48 -0400

    uploads and displays image

--- public/main.js
+++ public/main.js
@@ -1,6 +1,9 @@
 const upload = document.getElementById("upload");
+const image = document.getElementById("image");
 
 upload.addEventListener('change', (event) =>{
     const files = event.target.files;
+    image.src = URL.createObjectURL(files[0]);
+
     console.log(files);
 });
