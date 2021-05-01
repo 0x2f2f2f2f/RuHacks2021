e18f51af6679ea6070d1ce5aa01564df967d14c1
JerryHu <JerryHu87@github.com>
2021-05-01 15:27:46 -0400

    changing root directory to allow public folder

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ app.get("/",function(request,response){
     response.sendFile(__dirname+"/public/index.html");
 });
 
-express.static(public,[options])
 app.use(express.static('public'));
 app.listen(3000);
 console.log("displaying at http://localhost:3000");
