7f6c37a404e893cba679b462971b1d42707ea15e
JerryHu <JerryHu87@github.com>
2021-05-01 15:25:03 -0400

    changing root directory

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var express = require("express");
 var app = express();
 
 app.get("/",function(request,response){
-    express.static(root,[options])
-    app.use(express.static('public'));
-    response.sendFile(__dirname+"/index.html");
+    response.sendFile(__dirname+"/public/index.html");
 });
 
+express.static(root,[options])
+app.use(express.static('public'));
 app.listen(3000);
 console.log("displaying at http://localhost:3000");
