7f2359fc5eebf55f9baaa13aea381ea7ef10e3a1
JerryHu <JerryHu87@github.com>
2021-05-01 14:20:46 -0400

    changing index directory

--- node-input/server.js
+++ node-input/server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/",function(request,response){
-    response.sendFile(__dirname+"/views/index.html");
+    response.sendFile(__dirname+"/public/index.html");
 });
 
 app.listen(3000);
