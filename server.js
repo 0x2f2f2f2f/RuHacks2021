130603be8cb88bcc827cb176fa0bac46cbc0d830
Kai Xu <kaichengxu12@hotmail.com>
2021-05-02 16:56:38 -0400

    server responds when no face is detected

--- server.js
+++ server.js
@@ -19,13 +19,15 @@ const upload = multer({
 app.post('/upload', upload.single('file'), (req, res, next) => {
     const pythonProcess = spawn('python', ["./VisionAPI_Module.py", `./uploads/${req.file.filename}`]);
     pythonProcess.stdout.on('data', (data) => {
-        console.log(req.body.amount);
         pythonProcess.kill();
-        const secondPython = spawn('python', ["./Sentence_Generator_Module.py", data.toString().slice(0,-2), parseInt(req.body.amount)]);
-        secondPython.stdout.on('data', (finalData) => {
-            console.log(finalData.toString());
-            res.send(finalData.toString());
-        });
+        if (data.toString().slice(0,-2) == ""){
+            res.send("No Face Detected");
+        } else {
+            const secondPython = spawn('python', ["./Sentence_Generator_Module.py", data.toString().slice(0,-2), parseInt(req.body.amount)]);
+            secondPython.stdout.on('data', (finalData) => {
+                res.send(finalData.toString());
+            });
+        }
     });
 });
 
