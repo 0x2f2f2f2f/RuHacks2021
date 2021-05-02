66c48b2e58b3459b74ae499249fcfa8385ee921c
JerryHu <JerryHu87@github.com>
2021-05-02 00:53:38 -0400

    chnged fade animations

--- public/main.js
+++ public/main.js
@@ -1,8 +1,15 @@
 const upload = document.getElementById("upload");
 const image = document.getElementById("image");
 
+$('.first').addClass('animated fadeInUp');
+
+setTimeout(function () {
+    $('.second').show().addClass('animated fadeInUp');}, 2000
+);
+setTimeout(function () {
+    $('.third').show().addClass('animated fadeInUp');}, 4000
+);
 upload.addEventListener('change', (event) =>{
     const files = event.target.files;
     image.src = URL.createObjectURL(files[0]);
-
 });
