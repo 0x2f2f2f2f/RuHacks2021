9179ec064f275a2e115d82562e04fac900717da0
JerryHu <JerryHu87@github.com>
2021-05-02 16:02:03 -0400

    adding js file to about and neccessary components to head tag

---
+++ public/about.js
@@ -0,0 +1,6 @@
+setTimeout(function () {
+    $('.second').show().addClass('animated fadeInUp');}, 1000
+);
+setTimeout(function () {
+    $('.third').show().addClass('animated fadeInUp');}, 2000
+);
